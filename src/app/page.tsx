20ae857bb94c5432b96cc08c87025dcd540c9285
gxz <cnmmail163@163.com>
2024-10-19 17:54:22 +0800

    disabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,13 @@ import {useRouter} from "next/navigation";
 interface ItemProps {
     name: string
     url: string
+    disabled?: boolean
 }
 
-function Item({name, url}: ItemProps) {
+function Item({name, url, disabled}: ItemProps) {
     const {push} = useRouter()
     return (
-        <ListItemButton onClick={() => push(`./${url}`)}>
+        <ListItemButton onClick={() => push(`./${url}`)} disabled={disabled}>
             <ListItemText>{name}</ListItemText>
         </ListItemButton>
     )
@@ -20,7 +21,8 @@ function Item({name, url}: ItemProps) {
 const array: ItemProps[] = [
     {
         name: '理论研究和政策指导',
-        url: 'zhengce'
+        url: 'zhengce',
+        disabled: true
     },
     {
         name: '九大分区入口',
@@ -28,27 +30,33 @@ const array: ItemProps[] = [
     },
     {
         name: '专技预约与反馈',
-        url: 'zhuanji'
+        url: 'zhuanji',
+        disabled: true
     },
     {
         name: '特色课程指导',
-        url: 'zhidao'
+        url: 'zhidao',
+        disabled: true
     },
     {
         name: '经验分享区',
-        url: 'jingyan'
+        url: 'jingyan',
+        disabled: true
     },
     {
         name: '家长课堂',
-        url: 'jiazhang'
+        url: 'jiazhang',
+        disabled: true
     },
     {
         name: '问答区',
-        url: 'wenda'
+        url: 'wenda',
+        disabled: true
     },
     {
         name: '家庭互助讨论组',
-        url: 'huzhu'
+        url: 'huzhu',
+        disabled: true
     }
 ]
 
