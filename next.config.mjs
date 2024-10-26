3a25daa41b8c5736d4c678a3e76e3783dadf714a
gxz <cnmmail163@163.com>
2024-10-26 12:25:49 +0800

    extensions

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import createMDX from "@next/mdx"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = createMDX()({
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     experimental: {
         mdxRs: true
     }
